b7b303526d4280eeb5e56039294b58f81265fb1e
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-11-23 15:31:18 +0530

    email page fix

--- frontend/src/pages/EmailPage.tsx
+++ frontend/src/pages/EmailPage.tsx
@@ -3,22 +3,49 @@ import "./email.css";
 
 export default function EmailPage() {
   const [input, setInput] = useState("");
+  const [currentMode, setCurrentMode] = useState("email");
+  const [response, setResponse] = useState("");
   const handleSubmit = () => {
-    alert("Submitted" + input);
+    setResponse(input);
   };
 
   return (
-    <div className="emailpage">
+    <div className="emailpage" style={{ padding: "1rem" }}>
       <div className="tabs">
         <button className="tab active">Write</button>
         <button className="tab">Reply</button>
       </div>
       <div className="options">
-        <button className="option active">Email</button>
-        <button className="option">Essay</button>
-        <button className="option">Paragraph</button>
-        <button className="option">Idea</button>
-        <button className="option">Blog Post</button>
+        <button
+          className={`option ${currentMode === "email" ? "active" : ""}`}
+          onClick={() => setCurrentMode("email")}
+        >
+          Email
+        </button>
+        <button
+          className={`option ${currentMode === "letter" ? "active" : ""}`}
+          onClick={() => setCurrentMode("letter")}
+        >
+          Essay
+        </button>
+        <button
+          className={`option ${currentMode === "paragraph" ? "active" : ""}`}
+          onClick={() => setCurrentMode("paragraph")}
+        >
+          Paragraph
+        </button>
+        <button
+          className={`option ${currentMode === "idea" ? "active" : ""}`}
+          onClick={() => setCurrentMode("idea")}
+        >
+          Idea
+        </button>
+        <button
+          className={`option ${currentMode === "blog" ? "active" : ""}`}
+          onClick={() => setCurrentMode("blog")}
+        >
+          Blog Post
+        </button>
       </div>
       <div className="settings">
         Type:
@@ -37,6 +64,13 @@ export default function EmailPage() {
           Submit
         </button>
       </div>
+      {response && (
+        <div className="input-container" style={{ marginTop: "10px" }}>
+          <h2>Response: </h2>
+          <div className="footer response">{response}</div>
+          <div className="action-buttons"></div>
+        </div>
+      )}
     </div>
   );
 }
